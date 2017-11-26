56289cab6a25da7655ba455d43a9602ab9691084
b3m2a1 <b3m2a1@gmail.com>
2017-11-26 00:57:19 -0500

    Committed BTools application @ 2017-11-26_00:57:19

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,11 +1,12 @@
 (* Created with the Wolfram Language : www.wolfram.com *)
-Paclet[Name -> "BTools", Version -> "1.0.35", Creator -> "b3m2a1@gmail.com", 
+Paclet[Name -> "BTools", Version -> "1.0.36", Creator -> "b3m2a1@gmail.com", 
  Description -> "A general purpose package that implements useful \
 functionality for application   development including distribution, \
 documentation generation, formatting,    front-end manipulation, application \
 editing, etc.", Extensions -> {{"Kernel", "Root" -> ".", 
     "Context" -> {"BTools`"}, Root -> ".", Context -> {"BTools`"}}, 
    {"Resource", "Root" -> "Resources", "Resources" -> 
-     {"Templates", "Themes"}}, {"FrontEnd", "Prepend" -> True}}, 
+     {"PaletteGenerators", "Templates", "Themes"}}, 
+   {"FrontEnd", "Prepend" -> True}}, 
  Tags -> {"documentation", "front-end", "paclets", "web"}, 
  Categories -> "Development"]
