7c1890783dd471a65ea24ff2941ea1eadcecdec3
b3m2a1 <b3m2a1@gmail.com>
2019-04-29 18:12:30 -0700

    Committed BTools application @ 2019-04-29_18:12:30

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,6 +1,6 @@
 Paclet[
   Name -> "BTools",
-  Version -> "2.1.51",
+  Version -> "2.1.52",
   Creator -> "b3m2a1@gmail.com",
   URL -> "https://github.com/b3m2a1/mathematica-BTools",
   Description -> "A suite of development tools",
